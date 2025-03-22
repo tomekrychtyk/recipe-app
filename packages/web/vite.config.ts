4ad801a6891c4720f5091580edb12ce8d4e51b1f
Tomek Rychtyk <tomek@rychtyk.pl>
2025-03-22 19:32:42 +0100

    Added relative imports

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,10 +8,16 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
+      "/api": {
+        target: "http://localhost:4000",
         changeOrigin: true,
       },
     },
   },
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "../uicommon/src/assets"),
+    },
+  },
+});
