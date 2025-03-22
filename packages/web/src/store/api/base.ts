9c8ed8f8975b033691cf939d30e89bac4d30fb47
Tomek Rychtyk <tomek@rychtyk.pl>
2025-03-22 13:17:36 +0100

    refacotr

--- packages/web/src/store/api/base.ts
+++ packages/web/src/store/api/base.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4000/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
   tagTypes: ["Ingredient", "Meal"],
   endpoints: () => ({}),
 });
