50bc3d111e121e2ee4521c40231d04d916ec66ba
Tomek Rychtyk <tomek@rychtyk.pl>
2025-03-25 00:17:25 +0100

    Remove NVM

--- packages/server/src/routes/meals.ts
+++ packages/server/src/routes/meals.ts
@@ -62,7 +62,7 @@ router.get("/", async (_req, res) => {
   }
 });
 
-// trigger 6
+// trigger 7
 router.post("/", validateMeal, async (req, res) => {
   try {
     const { name, description, ingredients } = req.body;
