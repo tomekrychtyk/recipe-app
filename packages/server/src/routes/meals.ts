8b9834ddc1aa123d7a9cfefd918127169e8a97bf
Tomek Rychtyk <tomek@rychtyk.pl>
2025-03-24 23:44:11 +0100

    Fix CI/CD

--- packages/server/src/routes/meals.ts
+++ packages/server/src/routes/meals.ts
@@ -62,7 +62,7 @@ router.get("/", async (_req, res) => {
   }
 });
 
-// trigger 3
+// trigger 4
 router.post("/", validateMeal, async (req, res) => {
   try {
     const { name, description, ingredients } = req.body;
