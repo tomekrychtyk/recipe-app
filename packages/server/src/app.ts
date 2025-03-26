83ba1b15426ef8f231d27d7fa425b3cbffd6ab81
Tomek Rychtyk <tomek@rychtyk.pl>
2025-03-26 03:27:11 +0100

    Fix CORS issue on server

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -6,7 +6,30 @@ import foodDiaryRoutes from "./routes/foodDiary";
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://recipe-app-web.vercel.app",
+  "https://dabelo.pl",
+  "https://www.dabelo.pl",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/ingredients", ingredientsRoutes);
